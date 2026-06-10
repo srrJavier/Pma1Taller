b4e65ce3b17b598f98265bad8e0be1eda8c6100e
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-09 22:21:10 -0600

    015 Se agrego el ejercicio inversor de texto

--- src/org/javiersian/controllerStringManipulation/MenuStringManipulation.java
+++ src/org/javiersian/controllerStringManipulation/MenuStringManipulation.java
@@ -5,4 +5,5 @@ public class MenuStringManipulation {
 
     public String concatenarNombre(String nom, String ape) { return nom + " " + ape; }
     public int contadorLongitud(String texto) { return texto.length(); }
+    public String inversorTexto(String texto) { return new StringBuilder(texto).reverse().toString(); }
 }
