d66c4265c67d838237b218e5e3a1ea5c3a998ad3
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-09 22:16:27 -0600

    013 Se agrego el segundo modulo y el ejercicio(concatenar nombre)

---
+++ src/org/javiersian/controllerStringManipulation/MenuStringManipulation.java
@@ -0,0 +1,8 @@
+
+package org.javiersian.controllerStringManipulation;
+
+public class MenuStringManipulation {
+
+    public String concatenarNombre(String nom, String ape) { return nom + " " + ape; }
+    
+}
