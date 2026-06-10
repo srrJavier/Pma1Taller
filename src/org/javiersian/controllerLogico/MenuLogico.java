9076900667c84f90d367b6100197735a04705c5d
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-09 23:16:16 -0600

    026 se agrego ejercicio de semaforo

--- src/org/javiersian/controllerLogico/MenuLogico.java
+++ src/org/javiersian/controllerLogico/MenuLogico.java
@@ -3,4 +3,12 @@ package org.javiersian.controllerLogico;
 public class MenuLogico {
     public boolean verificadorEdad(int edad) { return edad >= 18; }
     public boolean esPar(int numero) { return numero % 2 == 0; }
+    public String semaforo(String color) {
+        return switch (color.toLowerCase()) {
+            case "rojo" -> "Detenerse";
+            case "amarillo" -> "Precaución";
+            case "verde" -> "Avanzar";
+            default -> "Color no válido";
+        };
+    }
 }
