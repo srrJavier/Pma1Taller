3de5eb53e56f7b5dda05118c2b0130924316e3e6
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-09 23:13:39 -0600

    025 se agrego ejercicio de numero par

--- src/org/javiersian/controllerLogico/MenuLogico.java
+++ src/org/javiersian/controllerLogico/MenuLogico.java
@@ -2,5 +2,5 @@
 package org.javiersian.controllerLogico;
 public class MenuLogico {
     public boolean verificadorEdad(int edad) { return edad >= 18; }
-    
+    public boolean esPar(int numero) { return numero % 2 == 0; }
 }
